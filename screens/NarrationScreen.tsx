c1b45edb7e948c6cad7cf70059505161adf2d56c
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-10-17 16:58:15 +0200

    NarrationScreen

--- screens/NarrationScreen.tsx
+++ screens/NarrationScreen.tsx
@@ -1,5 +1,5 @@
 import { narration } from '@drincs/pixi-vn';
-import { Box, Grid } from '@mui/system';
+import { Box, Grid, Stack } from '@mui/system';
 import { useQueryClient } from '@tanstack/react-query';
 import { INTERFACE_DATA_USE_QUEY_KEY, useQueryCanGoNext, useQueryDialogue } from '../use_query/useQueryInterface';
 import ChoiceMenu from './ChoiceMenu';
@@ -29,68 +29,73 @@ export default function NarrationScreen() {
     }
 
     return (
-        <Box
+        <Stack
+            direction="column"
+            spacing={0}
             sx={{
-                height: '95%',
+                justifyContent: "flex-end",
+                alignItems: "center",
+                height: '100%',
                 width: '100%',
                 position: "absolute",
                 left: 0,
                 right: 0,
-                top: 0,
+                bottom: 0,
             }}
         >
-            <ChoiceMenu
-                fullscreen={text ? false : true}
-            />
+            <ChoiceMenu />
             {text && <Box
                 sx={{
-                    position: "absolute",
-                    left: 0,
-                    right: 0,
-                    bottom: 0,
                     pointerEvents: "auto",
                     backgroundColor: "white",
-                    paddingTop: character?.name ? "20px" : "5px",
-                    paddingBottom: "5px",
                     height: "30%",
                     width: "100%",
                 }}
             >
-                <Grid
-                    container
-                    direction={"column"}
+                <Stack
+                    direction="column"
+                    spacing={0}
                     sx={{
-                        overflow: 'auto',
-                        marginRight: canGoNext ? "40px" : undefined,
-                        height: "100%",
+                        justifyContent: "flex-end",
+                        alignItems: "flex-start",
+                        height: '100%',
+                        width: '100%',
                     }}
                 >
-                    {character?.icon && <Grid size={2}>
-                        <img
-                            src={character?.icon}
-                            loading="lazy"
-                            alt=""
-                            style={{
-                                maxHeight: "100%",
-                                maxWidth: "100%",
-                            }}
-                        />
-                    </Grid>}
-                    <Grid size={character?.icon ? 10 : 12}>
-                        <Box>
-                            {text}
-                        </Box>
-                    </Grid>
                     {character && character.name && <div
                         style={{
-                            position: "absolute",
-                            top: 3,
-                            left: 5,
+                            marginLeft: "10px",
                         }}
                     >
                         {character.name + (character.surname ? " " + character.surname : "")}
                     </div>}
-                </Grid>
+                    <Grid
+                        container
+                        direction={"column"}
+                        sx={{
+                            overflow: 'auto',
+                            marginRight: canGoNext ? "40px" : undefined,
+                            height: "100%",
+                        }}
+                    >
+                        {character?.icon && <Grid size={2}>
+                            <img
+                                src={character?.icon}
+                                loading="lazy"
+                                alt=""
+                                style={{
+                                    maxHeight: "100%",
+                                    maxWidth: "100%",
+                                }}
+                            />
+                        </Grid>}
+                        <Grid size={character?.icon ? 10 : 12}>
+                            <Box>
+                                {text}
+                            </Box>
+                        </Grid>
+                    </Grid>
+                </Stack>
 
                 {canGoNext && <button
                     color="primary"
@@ -105,6 +110,6 @@ export default function NarrationScreen() {
                     Next
                 </button>}
             </Box>}
-        </Box>
+        </Stack>
     );
 }
