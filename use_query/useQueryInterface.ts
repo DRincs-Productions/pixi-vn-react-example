faea5d6de625fda91a5e2058f53c7c4737dc4eeb
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-10-21 09:40:37 +0200

    improve

--- use_query/useQueryInterface.ts
+++ use_query/useQueryInterface.ts
@@ -70,3 +70,23 @@ export function useQueryCanGoNext() {
 		},
 	});
 }
+
+const NARRATIVE_HISTORY_USE_QUEY_KEY = "narrative_history_use_quey_key";
+export function useQueryNarrativeHistory() {
+	return useQuery({
+		queryKey: [INTERFACE_DATA_USE_QUEY_KEY, NARRATIVE_HISTORY_USE_QUEY_KEY],
+		queryFn: () => {
+			return narration.narrativeHistory
+				.map((step) => {
+					let character = step.dialoge?.character ? getCharacterById(step.dialoge?.character) ?? new CharacterBaseModel(step.dialoge?.character, { name: step.dialoge?.character }) : undefined
+					return {
+						character: character?.name ? character.name + (character.surname ? " " + character.surname : "") : undefined,
+						text: step.dialoge?.text || "",
+						icon: character?.icon,
+						choices: step.choices,
+						inputValue: step.inputValue,
+					}
+				})
+		},
+	});
+}
