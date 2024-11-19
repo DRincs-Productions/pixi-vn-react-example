2118141933c4af9a0e0c46f3e6b1ac7b2036eb4b
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-11-19 16:52:43 +0100

    Add loading state to BackButton and NextButton components

--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -1,25 +1,30 @@
 import { narration } from '@drincs/pixi-vn';
 import { useQueryClient } from '@tanstack/react-query';
+import { useState } from 'react';
 import { INTERFACE_DATA_USE_QUEY_KEY, useQueryCanGoBack } from '../use_query/useQueryInterface';
 
 export default function BackButton() {
     const queryClient = useQueryClient()
     const { data: canGoBack = false } = useQueryCanGoBack()
+    const [loading, setLoading] = useState(false);
 
     async function backOnClick(): Promise<void> {
         try {
             if (!narration.canGoBack) {
                 return;
             }
+            setLoading(true);
             narration.goBack((_path) => {
                 // TODO: navigate in the url path
                 // READ THIS: https://pixi-vn.web.app/start/interface.html#navigate-switch-between-ui-screens
             })
                 .then(() => {
                     queryClient.invalidateQueries({ queryKey: [INTERFACE_DATA_USE_QUEY_KEY] })
+                    setLoading(false);
                 })
                 .catch((e) => {
                     console.error(e);
+                    setLoading(false);
                 })
             return;
         } catch (e) {
@@ -36,6 +41,7 @@ export default function BackButton() {
         <button
             color="primary"
             onClick={backOnClick}
+            disabled={loading}
             style={{
                 position: "absolute",
                 right: 0,
--- components/NextButton.tsx
+++ components/NextButton.tsx
@@ -1,22 +1,27 @@
 import { narration } from '@drincs/pixi-vn';
 import { useQueryClient } from '@tanstack/react-query';
+import { useState } from 'react';
 import { INTERFACE_DATA_USE_QUEY_KEY, useQueryCanGoNext } from '../use_query/useQueryInterface';
 
 export default function NextButton() {
     const queryClient = useQueryClient()
     const { data: canGoNext = false } = useQueryCanGoNext()
+    const [loading, setLoading] = useState(false);
 
     async function nextOnClick(): Promise<void> {
         try {
             if (!narration.canGoNext) {
                 return;
             }
+            setLoading(true);
             narration.goNext({})
                 .then(() => {
                     queryClient.invalidateQueries({ queryKey: [INTERFACE_DATA_USE_QUEY_KEY] })
+                    setLoading(false);
                 })
                 .catch((e) => {
                     console.error(e);
+                    setLoading(false);
                 })
             return;
         } catch (e) {
@@ -33,6 +38,7 @@ export default function NextButton() {
         <button
             color="primary"
             onClick={nextOnClick}
+            disabled={loading}
             style={{
                 position: "absolute",
                 right: 0,
